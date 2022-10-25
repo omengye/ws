496ecd6230addb7a5d91c24273a72d6d8f50ea18
omengye <ym2008love@gmail.com>
2022-10-25 23:49:49 +0800

    add feign convert bean

--- gcs/src/main/java/io/omengye/gcs/configure/SecurityConfig.java
+++ gcs/src/main/java/io/omengye/gcs/configure/SecurityConfig.java
@@ -5,8 +5,12 @@ import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import com.nimbusds.jose.proc.JWKSecurityContext;
 import com.nimbusds.jose.proc.JWSVerificationKeySelector;
 import com.nimbusds.jwt.proc.ConfigurableJWTProcessor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.jose.jws.SignatureAlgorithm;
@@ -15,6 +19,7 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 import reactor.core.publisher.Flux;
 
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 @EnableWebFluxSecurity
 public class SecurityConfig {
@@ -29,6 +34,12 @@ public class SecurityConfig {
 				.oauth2ResourceServer()
 				.jwt();
 		return http.build();
+	}
+
+	@Bean
+	@ConditionalOnMissingBean
+	public HttpMessageConverters messageConverters(ObjectProvider<HttpMessageConverter<?>> converters) {
+		return new HttpMessageConverters(converters.orderedStream().collect(Collectors.toList()));
 	}
 
 }
--- gcs/src/main/java/io/omengye/gcs/feign/UserInfoFeignService.java
+++ gcs/src/main/java/io/omengye/gcs/feign/UserInfoFeignService.java
@@ -1,14 +1,14 @@
 package io.omengye.gcs.feign;
 
 
-import io.omengye.gcs.feign.impl.UserInfoServiceFallbackFactory;
+import io.omengye.gcs.feign.impl.UserInfoServiceFallback;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
-@FeignClient(name = "ws-userinfo", url="${userinfo.user.url}", fallbackFactory = UserInfoServiceFallbackFactory.class)
+@FeignClient(name = "ws-userinfo", url="${userinfo.user.url}", fallback = UserInfoServiceFallback.class)
 public interface UserInfoFeignService  {
 
     @GetMapping("/visit")
